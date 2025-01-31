31d602ab6e7e3f9940c52a6794175f75fda12cd5
argi <15852038+argium@users.noreply.github.com>
2025-02-01 00:25:12 +1100

    Initial commit

---
+++ Stats/Program.cs
@@ -0,0 +1,23 @@
+﻿using Linq2GraphQL.Client;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using WarcraftLogs.Public;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// create a service container
+var serviceCollection = new ServiceCollection();
+serviceCollection
+    .AddWarcraftLogsPublicClient(opt => {})
+    .WithHttpClient(
+        httpClient => 
+        { 
+            httpClient.BaseAddress = new Uri("https://spacex-production.up.railway.app/"); 
+        });
+serviceCollection.AddLogging(c => c.AddConsole());
+
+var services = serviceCollection.BuildServiceProvider();
+
+var client = services.GetRequiredService<WarcraftLogsPublicClient>();
+await client.Query.CharacterData().Select(c => c.Character.Name).ExecuteAsync();
---
+++ Stats/README.md
@@ -0,0 +1,3 @@
+
+
+`dotnet Linq2graphQl https://www.warcraftlogs.com/api/v2/client -c "WarcraftLogsPublicClient" -n "WarcraftLogs.Public" -o Generated --token $t`
