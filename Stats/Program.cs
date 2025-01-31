3b92dc3104675d602cc060c890c49a2fc84e36d8
argi <15852038+argium@users.noreply.github.com>
2025-02-01 01:32:19 +1100

    It works

--- Stats/Program.cs
+++ Stats/Program.cs
@@ -1,23 +1,150 @@
-﻿using Linq2GraphQL.Client;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using WarcraftLogs.Public;
+using GraphQL.Client.Http;
+using GraphQL.Client.Abstractions.Websocket;
+using GraphQL;
+using GraphQL.Client.Serializer.SystemTextJson;
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+IHostEnvironment env = builder.Environment;
 
-// create a service container
-var serviceCollection = new ServiceCollection();
-serviceCollection
-    .AddWarcraftLogsPublicClient(opt => {})
-    .WithHttpClient(
-        httpClient => 
-        { 
-            httpClient.BaseAddress = new Uri("https://spacex-production.up.railway.app/"); 
-        });
-serviceCollection.AddLogging(c => c.AddConsole());
+string token = string.Empty;
 
-var services = serviceCollection.BuildServiceProvider();
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true);
 
-var client = services.GetRequiredService<WarcraftLogsPublicClient>();
-await client.Query.CharacterData().Select(c => c.Character.Name).ExecuteAsync();
+builder.Services
+	.AddLogging(c => c.AddConsole())
+	.AddScoped<IGraphQLWebsocketJsonSerializer, SystemTextJsonSerializer>()
+	.AddScoped<IGraphQLWebSocketClient, GraphQLHttpClient>(sp => {
+		var httpClient = new HttpClient(new LoggingHandler(new HttpClientHandler()));
+		httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+		var client = new GraphQLHttpClient("https://www.warcraftlogs.com/api/v2/client", new SystemTextJsonSerializer(), httpClient);
+		return client;
+	});
+
+IHost app = builder.Build();
+
+var log = app.Services.GetRequiredService<ILogger<Program>>();
+log.LogInformation("Starting application");
+
+
+var graphQLClient = app.Services.GetRequiredService<IGraphQLWebSocketClient>();
+var heroRequest = new GraphQLRequest {
+    Query = """
+		query ($name: String!, $serverSlug: String!, $serverRegion: String!) {
+			rateLimitData {
+				limitPerHour
+				pointsSpentThisHour
+				pointsResetIn
+			}
+			characterData {
+				character(name: $name, serverSlug: $serverSlug, serverRegion: $serverRegion) {
+					name
+					id
+					classID
+					recentReports(limit: 1) {
+						data {
+							fights(killType: Encounters) {
+								encounterID
+								name
+								endTime
+							}
+						}
+					}
+				}
+			}
+		}
+	""",
+	Variables = new
+	{
+		name = "Argium",
+		serverSlug = "frostmourne",
+		serverRegion = "us"
+	}
+};
+
+var reportsData = new GraphQLRequest {
+    Query = """
+		query ($guildName: String!, $guildServerSlug: String!, $guildServerRegion: String!) {
+			rateLimitData {
+				limitPerHour
+				pointsSpentThisHour
+				pointsResetIn
+			}
+			guildData {
+				guild(name: $guildName, serverSlug: $guildServerSlug, serverRegion: $guildServerRegion) {
+					name
+					id
+				}
+			}
+			reportData {
+				reports(guildName: $guildName, guildServerSlug: $guildServerSlug, guildServerRegion: $guildServerRegion, limit:5) {
+					data {
+						code
+						fights {
+							name
+						}
+					}
+				}
+			}
+		}
+	""",
+	Variables = new
+	{
+		guildName = "Resus",
+		guildServerSlug = "frostmourne",
+		guildServerRegion = "us"
+	}
+};
+
+
+// var graphQLResponse = await graphQLClient.SendQueryAsync<dynamic>(heroRequest);
+var graphQLResponse = await graphQLClient.SendQueryAsync<dynamic>(reportsData);
+
+log.LogInformation($"GraphQL Response: {graphQLResponse}");
+
+// var client = app.Services.GetRequiredService<WarcraftLogsPublicClient>();
+// var guild = await client
+// 	.Query
+// 	.GuildData()
+// 	.Select(x => x)
+// 	.ExecuteAsync();
+
+// log.LogInformation($"Guild: {guild}");
+
+
+public class LoggingHandler : DelegatingHandler
+{
+    public LoggingHandler(HttpMessageHandler innerHandler)
+        : base(innerHandler)
+    {
+    }
+
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Request:");
+        Console.WriteLine(request.ToString());
+        if (request.Content != null)
+        {
+            Console.WriteLine(await request.Content.ReadAsStringAsync());
+        }
+        Console.WriteLine();
+
+        HttpResponseMessage response = await base.SendAsync(request, cancellationToken);
+
+        Console.WriteLine("Response:");
+        Console.WriteLine(response.ToString());
+        if (response.Content != null)
+        {
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+        }
+        Console.WriteLine();
+
+        return response;
+    }
+}
